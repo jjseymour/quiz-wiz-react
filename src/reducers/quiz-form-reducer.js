3ad4e6fca9f35ba8fda1d4546da6a508f1f34e91
JJ Seymour <jjseymour@gmail.com>
2017-02-26 18:23:32 -0500

    refactored ADD_ANSWER reducer statement

--- src/reducers/quiz-form-reducer.js
+++ src/reducers/quiz-form-reducer.js
@@ -12,17 +12,25 @@ export default function(state = null, action){
       })
       return {...state, questions_attributes: questionObject}
     case 'ADD_ANSWER':
-      const question = state.questions_attributes[action.payload.questionId]
-      const questionArr = state.questions_attributes.filter(question=>question.id === action.payload.questionId)
-      const answerObject = {...question.possible_answers_attributes[parseInt(action.payload.id)]}
-      const answerArray = question.possible_answers_attributes.filter(answer => answer.id === parseInt(action.payload.id))
-      answerObject[action.payload.answer_type] = action.payload[action.payload.answer_type]
-      if (action.payload.content) answerObject.content = action.payload.content
-      if (action.payload.code_mirror_language) answerObject.code_mirror_language = action.payload.code_mirror_language
-      answerObject.answer_type = action.payload.answer_type
-      let rebuiltQuestion = {...question, possible_answers_attributes: [...answerArray, answerObject]}
-      let rebuiltQuestionArray = [...questionArr, rebuiltQuestion]
-      return {...state, questions_attributes: rebuiltQuestionArray} 
+      const questions = state.questions_attributes.map((question) => {
+        if (question.inputValue !== `input-${action.payload.questionId}`) {
+          return question
+        }else {
+          const answers = question.possible_answers_attributes.map((answer) => {
+            if (answer.inputValue !== `answerInput-${parseInt(action.payload.id)}`) {
+              return answer
+            }else {
+              answer.answer_type = action.payload.answer_type
+              answer[action.payload.answer_type] = action.payload[action.payload.answer_type]
+              if (action.payload.content) answer.content = action.payload.content
+              if (action.payload.code_mirror_language) answer.code_mirror_language = action.payload.code_mirror_language
+              return answer
+            }
+          }, action.payload)
+          return {...question, possible_answers_attributes: answers}
+        }
+      })
+      return {...state, questions_attributes: questions} 
     case 'ADD_ANSWERS_INPUT':
       let newState = Object.assign({}, state, {questions_attributes: [...state.questions_attributes]})
       newState.questions_attributes[action.payload.id].possible_answers_attributes = action.payload.possible_answers_attributes
