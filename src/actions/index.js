78d0a7f0674caae92c84d59c3e575a7bef69d539
JJ Seymour <jjseymour@gmail.com>
2017-06-21 14:58:17 -0400

    move packages and set process.env.NODE_ENV

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {browserHistory} from 'react-router';
 
-export const url = ENV['URL'] || 'http://localhost:3000/';
+export const url = process.env.URL || 'http://localhost:3000/';
 
 const getUserToken = () => {
   return axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('jwt')
